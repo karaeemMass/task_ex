243e40fc37a35d4e9ec84d8471d90a6eb72f6428
kareemMas <kareem_arch@mas-soft.com>
2026-01-13 15:49:42 +0200

    version 1

--- internal/interceptor/auth.go
+++ internal/interceptor/auth.go
@@ -15,7 +15,9 @@ func AuthInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServe
 	if !ok {
 		return nil, status.Errorf(codes.Unauthenticated, "لا يوجد بيانات Metadata")
 	}
+
 	//fmt.Printf("Incoming Metadata: %v\n", md) // Debugging line to print incoming metadata
+
 	values := md["authorization"]
 	if len(values) == 0 || values[0] != "secret-key" {
 		return nil, status.Errorf(codes.Unauthenticated, "التوكن غير صحيح أو مفقود")
