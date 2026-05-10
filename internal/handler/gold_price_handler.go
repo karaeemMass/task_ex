65fe23f5af420580abece8abc8fa40a32cb8465f
kareemMas <kareem_arch@mas-soft.com>
2026-05-10 16:07:11 +0300

    +

--- internal/handler/gold_price_handler.go
+++ internal/handler/gold_price_handler.go
@@ -1,18 +1,37 @@
+/*
 package handler
 
 import (
+
 	"fmt"
 	"task_ex/internal/service"
 	"task_ex/service/pb"
+
 )
-type GoldPrice struct{
+
+	type GoldPrice struct{
+		pb.UnimplementedTaskServiceServer
+		Goldservice *service.GetGoldPrice
+	}
+
+	func newGoldPriceHandler (Goldservice *service.GetGoldPrice) *GoldPrice{
+		fmt.Println("f")
+		return &GoldPrice{Goldservice: Goldservice}
+
+}
+*/
+package handler
+
+import (
+	"task_ex/internal/service"
+	"task_ex/service/pb"
+)
+
+type GoldPrice struct {
 	pb.UnimplementedTaskServiceServer
-	Goldservice *service.GetGoldPrice
-} 
+	Goldservice service.GetGoldPriceFunc
+}
 
-
-func newGoldPriceHandler (Goldservice *service.GetGoldPrice ) *GoldPrice{
-	fmt.Println("f")
-	return &GoldPrice{Goldservice: Goldservice}
-	
+func newGoldPriceHandler(gs service.GetGoldPriceFunc) *GoldPrice {
+	return &GoldPrice{Goldservice: gs}
 }
